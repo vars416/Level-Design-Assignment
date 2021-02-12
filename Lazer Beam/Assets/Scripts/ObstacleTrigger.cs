699f2a8da688ffe3bc4f275bcd5f7052caf51362
vars416 <56270900+vars416@users.noreply.github.com>
2021-02-12 18:50:18 -0500

    Script added (trigger based)

---
+++ Lazer Beam/Assets/Scripts/ObstacleTrigger.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ObstacleTrigger : MonoBehaviour
+{
+
+    public GameObject Trigger;
+    public GameObject Obstacle;
+    private bool IsItHappening;
+    public float Timer;
+    private float decreasing;
+    // Start is called before the first frame update
+    void Start()
+    {
+        IsItHappening = false;
+        decreasing = Timer;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        TurningOff();
+        TurningOn();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        print(other);
+        if (Trigger.GetComponent<Collider>() == other)
+        {
+            IsItHappening = true;
+            print(IsItHappening);
+        }
+    }
+    void TurningOff ()
+    {
+        if (IsItHappening == true)
+        {
+            if (decreasing > 0f)
+            {
+                decreasing -= Time.deltaTime;
+                //print(decreasing);
+            }
+            Obstacle.gameObject.SetActive(false);
+        }
+    }
+
+    void TurningOn ()
+    {
+        if (decreasing <= 0f)
+        {
+            Obstacle.gameObject.SetActive(true);
+            IsItHappening = false;
+            print(IsItHappening);
+            decreasing = Timer;
+        }
+    }
+}
